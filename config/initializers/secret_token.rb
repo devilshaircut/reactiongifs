cb42daf4c0e861b05fe4aa6f3d2d3a204699f38f
Jason Bishop <devilshaircut@gmail.com>
2012-04-03 15:28:50 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reactiongifs::Application.config.secret_token = '491215b2b2b3a18193414413e0f70ba841d886c1f8031e82f528c2bf45ad9eb0ee02ad27b1b7bb7cf49d7e6e6736f67d55c7abb3abfcaf032729f4401e98741a'
