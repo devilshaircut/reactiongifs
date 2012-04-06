b14163da59b49e99888e704f4a6fd6d36133ccac
Jason Bishop <devilshaircut@gmail.com>
2012-04-06 11:27:15 -0400

    Precompile for Cedar.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,8 @@ Reactiongifs::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  # config.assets.compile = false
-  config.assets.compile = true
+  config.assets.precompile += ["bootstrap/bootstrap", "bootstrap/bootstrap-responsive", "reactiongifs", "jquery", "bootstrap/bootstrap"]
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
