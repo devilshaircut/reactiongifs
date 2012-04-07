539e03bef7f63483e82a2c0c4b0f5b3de953cf93
Jason Bishop <devilshaircut@gmail.com>
2012-04-06 20:33:48 -0400

    Create the UI for search. Also add in comments for code readability.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,11 @@ class HomeController < ApplicationController
 
   def index
     
+    # Enable to the index page to show a random assortment of GIFs.
+    # Select a random number from 1 to the number of Image records.
     imagecount = Image.count
     @imagearray = []
+    # If a record exists with ID matching the RNG, add it to the array. Do this until array is length 8.
     until @imagearray.count == 8 do
       imageid = rand(imagecount) + 1
       unless @imagearray.include?(imageid) || Image.find_by_id(imageid) == nil
@@ -15,13 +18,13 @@ class HomeController < ApplicationController
   
   def search
     
-    imagecount = Image.count
-    @imagearray = []
-    until @imagearray.count == 8 do
-      imageid = rand(imagecount) + 1
-      unless @imagearray.include?(imageid) || Image.find_by_id(imageid) == nil
-        @imagearray += [imageid]
-      end
+    # Enable search for Index and Search pages.
+    # Make sure the user submits something usable for search. Return Image records in an array.
+    if params[:search] != nil && Tag.find_by_keyword(params[:search]) != nil && params[:search].strip != ""
+      @searchresult = Tag.find_by_keyword(params[:search]).images
+    # If the user has submitted bad data, then return nil.
+    else
+      @searchresult = nil
     end
     
   end
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -10,23 +10,33 @@ class ImagesController < ApplicationController
   
   def create
     
+    # Create a new Image record from user-submitted data (including URL, description, and keywords).
+    # Start creating a new image record.
     @image = Image.new
+    # Make sure the user has submitted valid data.
     if params[:image] != nil && params[:image]["url"].strip != ""
+      # Make sure a record for this image does not already exist. Edit URL and description.
       if Image.find_by_url(params[:image]["url"]) == nil
         @image.url = params[:image]["url"]
         @image.description = params[:image]["description"]
         @image.save
+        # Only create Tag records if the user has submitted tag data.
         if params[:image]["tags"] != nil
+          # Split comma-separated string and remove whitespace. From the created array, create Tag records.
           tagsarray = params[:image]["tags"].split(",").collect { |c| c.strip }
+          # Create the Image record's tags. Simultaneously, build the relationship in the join table.
           tagsarray.each do |t|
             @image.tags << Tag.find_or_create_by_keyword(t)
           end
         end
+        # The record has been created; redirect user to a page with the image in it.
         redirect_to image_path(@image)
       else
+        # The record already exists; redirect user back to the original form.
         redirect_to new_image_path, :alert => "This image already exists."
       end
     else
+      # The user submitted bad data; redirect user back to the original form.
       redirect_to new_image_path, :alert => "All fields must be complete."
     end
 
@@ -37,11 +47,14 @@ class ImagesController < ApplicationController
   
   def index
     
+    # Enable to the image index page to show a random assortment of GIFs.
+    # Select a random number from 1 to the number of Image records.
     imagecount = Image.count
     @imagearray = []
-    until @imagearray.count == 4 do
+    # If a record exists with ID matching the RNG, add it to the array. Do this until array is length 8.
+    until @imagearray.count == 8 do
       imageid = rand(imagecount) + 1
-      unless @imagearray.include?(imageid)
+      unless @imagearray.include?(imageid) || Image.find_by_id(imageid) == nil
         @imagearray += [imageid]
       end
     end
@@ -49,3 +62,21 @@ class ImagesController < ApplicationController
   end
   
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
