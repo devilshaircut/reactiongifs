0e8663e2ee00d9044846195c64da72c1b4e03995
Jason Bishop <devilshaircut@gmail.com>
2012-04-10 21:48:53 -0400

    Fix seeds; add Devise routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Reactiongifs::Application.routes.draw do
   
-  devise_for :users
+  devise_for :users,
+    :path         => "users",
+    :path_names   => {
+      :sign_in        =>  'login',
+      :sign_out       =>  'logout',
+      :password       =>  'password',
+      :confirmation   =>  'verification',
+      :unlock         =>  'unblock',
+      :registration   =>  'register',
+      :sign_up        =>  'signup'
+    }
 
   root :to            =>  "home#index"
   match '/search'     =>  'home#search'
