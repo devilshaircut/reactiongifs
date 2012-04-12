c9049d571d1c25f52bf5adc0bff28ad81d6768c3
Jason Bishop <devilshaircut@gmail.com>
2012-04-12 01:42:10 -0400

    Remove registration on production app.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  if Rails.env.production?
+   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  else
+   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  end
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
